db1b5285be6d7a2f3fc7033f390ee84e0d1e08b7
Alex Zaremba <sashadjram@gmail.com>
2022-01-03 22:21:13 +0300

    add Test.java

---
+++ lecture23_final_project/Test.java
@@ -0,0 +1,7 @@
+package ru.sendgoods.otus.java_developer_basic_2021_homeworks.lecture23_final_project;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
